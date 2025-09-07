cb5f7c000b0bb64b11e871d6a01822c1734a96f6
Kenneth <hi@chatkenneth.com>
2025-09-07 10:49:48 +0800

    add font

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -80,6 +80,7 @@
                                                <?php while ( $all_websites_query->have_posts() ) : $all_websites_query->the_post(); ?>
                                                    <div class="col-12 col-lg-12">
                                                         <?php $acf_all_entries = get_field('all_entries'); ?>
+                                                        <?php $acf_website_data_url = get_field('website_data_url'); ?>
                                         
 
                                                         <?php if($acf_all_entries): ?>
@@ -97,8 +98,17 @@
                                                            <?php endforeach; ?>
                                                         <?php endif; ?>
 
-                                                        <h2><?php the_title(); ?></h2>
-                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.</p>
+                                                        <div class="mt-3">
+                                                            <div class="row align-items-center justify-content-between gy-3 mb-2">
+                                                                <div class="col-auto">
+                                                                    <h2 class="h3"><?php the_title(); ?></h2>
+                                                                </div>
+                                                                <div class="col-auto">
+                                                                    <a href="<?php echo $acf_website_data_url; ?>" target="_blank">Visit Site <i class="fa-regular fa-fw fa-arrow-right-long fa-f178"></i></a>
+                                                                </div>
+                                                            </div>
+                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat.</p>
+                                                        </div>
                                                    </div>
                                                <?php endwhile; ?>
                                            
