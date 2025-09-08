ddf184716f0747b6e2235a2dad1f538040d92a25
Kenneth <hi@chatkenneth.com>
2025-09-08 09:40:28 +0800

    add cpint

--- template-parts/content-all-fonts.php
+++ template-parts/content-all-fonts.php
@@ -8,7 +8,7 @@
 
 <?php switch_to_blog($main_blog); ?>
 
-   <?php $term_args  = array('taxonomy' => 'all_websites_font_family','hide_empty' => false,'parent' => 0 , 'exclude' => array('1'),'fields' => 'names'); ?>
+   <?php $term_args  = array('taxonomy' => 'all_websites_font_family','hide_empty' => false,'parent' => 0 , 'exclude' => array('1'),'fields' => 'all'); ?>
    <?php # https://developer.wordpress.org/reference/functions/get_terms/ ?>
    <?php # fields options: 'names|ids|all' ?>
    <?php $term_array = get_terms($term_args); ?>
@@ -20,7 +20,7 @@
                  <a href="javascript:void(0)" data-remodal-action="close"  class="d-block p-3 border">
                      <?php $image_placeholder = $image_path . $all_items_ctr.'.webp'; ?>
                      <!-- <img src="<?php echo $image_placeholder; ?>" title="800x800" alt="800x800" class="border"> -->
-                     <span class="h3 text-center"><?php echo $each_item; ?></span>
+                     <span class="h3 text-center"><?php echo $each_item->name; ?> (<?php echo $each_item->count; ?>)</span>
                  </a>
                 
              </div>
